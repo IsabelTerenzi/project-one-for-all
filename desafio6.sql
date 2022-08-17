513a94d88816f0a0e76d08deb4fac0dba4c8eff6
Isabel Terenzi <bel.terenzi@gmail.com>
2022-08-17 19:21:51 -0300

    requisito 6

---
+++ desafio6.sql
@@ -0,0 +1,5 @@
+SELECT MIN(plan.price) AS faturamento_minimo,
+MAX(plan.price) AS faturamento_maximo,
+ROUND(AVG(plan.price), 2) AS faturamento_medio,
+SUM(plan.price) AS faturamento_total
+FROM SpotifyClone.plan AS plan INNER JOIN SpotifyClone.user ON plan.plan_id = user.plan_id;
